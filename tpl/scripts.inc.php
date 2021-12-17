6f700efff77467ca091bdbf80d0bf8ba34ae5f02
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-18 00:46:58 +0300

    update

--- tpl/scripts.inc.php
+++ tpl/scripts.inc.php
@@ -2,28 +2,28 @@
 <script data-wb-src="engine" data-wb-append="body"></script>
 <script data-wb-src="plugins" data-wb-append="body"></script>
 <script>
-$(document).on('wbapp',function(){
-    wbapp.loadScripts([
-        "/tpl/src/js/popper.min.js",
-        "/tpl/src/js/bootstrap.min.js",
-        "/tpl/src/js/iscroll-lite.js",
-        "/tpl/src/js/slick.min.js",
-        "/tpl/build/libs/raty/jquery.raty.min.js",
-        "/tpl/build/libs/fotorama/fotorama.js",
-        "/tpl/build/libs/bootstrap-select/bootstrap-select.min.js",
-        "/tpl/build/libs/classie.js",
-        "/tpl/src/js/svg4everybody.min.js",
-        "/tpl/src/js/moment-ru.min.js",
-        "/tpl/src/js/bootstrap-datetimepicker.min.js",
-        "/tpl/src/js/owl.carousel.min.js",
-        "/tpl/src/js/jquery.ui.touch-punch.min.js",
-        "/tpl/src/js/lightcase.js"
-],'ready',function(){
-    wbapp.loadScripts(["/tpl/src/js/scripts.js"]);
-})
-});
+    $(document).on('wbapp', function() {
+        setTimeout(function() {
+            wbapp.loadScripts([
+                "/tpl/src/js/popper.min.js",
+                "/tpl/src/js/bootstrap.min.js",
+                "/tpl/src/js/iscroll-lite.js",
+                "/tpl/src/js/slick.min.js",
+                "/tpl/build/libs/raty/jquery.raty.min.js",
+                "/tpl/build/libs/fotorama/fotorama.js",
+                "/tpl/build/libs/bootstrap-select/bootstrap-select.min.js",
+                "/tpl/build/libs/classie.js",
+                "/tpl/src/js/svg4everybody.min.js",
+                "/tpl/src/js/moment-ru.min.js",
+                "/tpl/src/js/bootstrap-datetimepicker.min.js",
+                "/tpl/src/js/owl.carousel.min.js",
+                "/tpl/src/js/jquery.ui.touch-punch.min.js",
+                "/tpl/src/js/lightcase.js"
+            ], 'ready', function() {
+                wbapp.loadScripts(["/tpl/src/js/scripts.js"]);
+            })
+        }, 1500)
+    });
 </script>
 <script src="https://api-maps.yandex.ru/2.0/?load=package.full&lang=ru-RU"></script>
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-
-
